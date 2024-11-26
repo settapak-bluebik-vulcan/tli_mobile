ad45e3c96d08186b66baf26ccc4868bb969cc7bf
icezerio <icezerio.developer@gmail.com>
2024-11-26 18:00:08 +0700

    chore: init react native health connect

---
+++ android/app/src/main/java/com/tli_mobile/PermissionRationaleActivity.kt
@@ -0,0 +1,27 @@
+package com.tli_mobile
+
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class PermissionRationaleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        }
+
+        webView.loadUrl("https://developer.android.com/health-and-fitness/guides/health-connect/develop/get-started")
+
+        setContentView(webView)
+    }
+}
