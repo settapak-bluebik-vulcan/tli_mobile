1393bf8b1b473127e1901c2f7189a73e8d33fa3f
PhoatipongMu <phoatipong.mu@vulcan.bluebik.com>
2024-11-26 09:52:57 +0700

    chore: delete paper, move App into src

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
-import { DefaultTheme, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { name as appName } from './app.json';
-import App from './App';
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    secondary: 'yellow',
-  },
-};
+import App from './src/App';
 
 export default function Main() {
   return (
     <SafeAreaProvider>
-      <PaperProvider theme={theme}>
-        <App />
-      </PaperProvider>
+      <App />
     </SafeAreaProvider>
   );
 }
